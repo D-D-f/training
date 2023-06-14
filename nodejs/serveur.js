3095bd220821f544d274d35f30e07cbf91692f51
D-D-F <defreitas-david@outlook.fr>
2023-06-14 16:04:26 +0200

    add routing

--- nodejs/serveur.js
+++ nodejs/serveur.js
@@ -7,9 +7,25 @@ server.on("request", (req, res) => {
   res.writeHead(200, {
     "content-types": "text/html",
   });
-  const fileContent = fs.readFileSync("./index.html", "utf-8");
-  const template = fileContent.replace("{{name}}", "David");
-  res.end(template);
+  const url = req.url;
+  let fileContent;
+  if (url === "/home") {
+    res.writeHead(200, {
+      "content-types": "text/html",
+    });
+    fileContent = fs.readFileSync("./index.html", "utf-8");
+  } else if (url === "/info") {
+    res.writeHead(200, {
+      "content-types": "text/html",
+    });
+    fileContent = fs.readFileSync("./info.html", "utf-8");
+  } else {
+    res.writeHead(404, {
+      "content-types": "text/html",
+    });
+    fileContent = fs.readFileSync("./notfound.html", "utf-8");
+  }
+  res.end(fileContent);
 });
 
 server.listen(8080);
