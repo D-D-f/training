1666021bc0be8d86bbb013e209e06653c88c7ab3
D-D-F <defreitas-david@outlook.fr>
2023-06-15 14:30:52 +0200

    update express

--- nodejs/express.js
+++ nodejs/express.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.writeHead(200, { "content-type": "text/plain" });
-  res.end("Hello world");
+  const file = fs.readFileSync("./index.html", "utf-8");
+  res.send(file);
+  res.end();
 });
 
 app.listen(3000);
