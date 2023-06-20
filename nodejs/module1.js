9edaaaf2ab330922463319ea35c7eef77f97aa9a
D-D-F <defreitas-david@outlook.fr>
2023-06-20 14:00:40 +0200

    update template

--- nodejs/module1.js
+++ nodejs/module1.js
@@ -1,12 +1,24 @@
 const fs = require("fs");
 const express = require("express");
 const app = express();
+const path = require("path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  console.log(req.headers);
-  const fileContent = fs.readFileSync("index.html", "utf-8");
-  res.send(fileContent);
-  res.end();
+  res.render("app", {
+    name: "David",
+    authentificated: true,
+    friends: 11,
+    products: [
+      { title: "product1", content: "content1" },
+      { title: "product2", content: "content2" },
+      { title: "product3", content: "content3" },
+    ],
+  });
 });
 
 app.listen(3000);
+
+// Il faut installer ejs via pnpm
