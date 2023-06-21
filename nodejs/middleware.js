1d2a1d8b784753a8142beb92485d65225ff04dac
D-D-F <defreitas-david@outlook.fr>
2023-06-21 15:55:12 +0200

    update middleware

--- nodejs/middleware.js
+++ nodejs/middleware.js
@@ -4,17 +4,37 @@ const path = require("path");
 const fs = require("fs");
 const app = express();
 
-const middleware = (req, res, next) => {
-  console.log("coucou");
-  next();
-};
-app.use(middleware); // je peux ajouter ou enlever en premier parametre un chemin
-// app.get('*", middleware) cela ciblera toutes les requetes get
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+const getUser = (req, res, next) => {
+  req.user = {
+    name: "toto",
+    authenticated: false,
+  };
+  next();
+};
+
+const isAuthenticated = (req, res, next) => {
+  if (req.user.authenticated) {
+    console.log("user ok");
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+};
+
+app.use("/index", getUser, isAuthenticated);
+
+// je peux ajouter ou enlever en premier parametre un chemin
+// app.get('*", middleware) cela ciblera toutes les requetes get
+
 app.get("/index", (req, res) => {
   res.render("ok");
+});
+
+app.get("/index", (req, res) => {
+  res.sendStatus(403);
 });
 
 app.listen(3000);
