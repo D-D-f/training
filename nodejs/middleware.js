9880e5d37246d92cde3b6e13b058ec413a6e4944
D-D-F <defreitas-david@outlook.fr>
2023-06-21 15:28:20 +0200

    update middleware

--- nodejs/middleware.js
+++ nodejs/middleware.js
@@ -1,4 +1,4 @@
-// Le middleware peut s'éxecuter pour chaque requete ou alors pour une fonction ou pour un chemin
+// Le middleware peut s'éxecuter pour chaque requete ou alors pour une method ou pour un chemin
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
@@ -8,8 +8,8 @@ const middleware = (req, res, next) => {
   console.log("coucou");
   next();
 };
-app.use("/index", middleware);
-
+app.use(middleware); // je peux ajouter ou enlever en premier parametre un chemin
+// app.get('*", middleware) cela ciblera toutes les requetes get
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
