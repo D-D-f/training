db4c46418315f3b55de10de64f1494bf3439c64f
D-D-F <defreitas-david@outlook.fr>
2023-06-23 10:47:10 +0200

    update middleware

--- nodejs/middleware.js
+++ nodejs/middleware.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const app = express();
+const morgan = require("morgan");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -10,7 +11,7 @@ app.set("view engine", "ejs");
 const getUser = (req, res, next) => {
   req.user = {
     name: "david",
-    authenticated: false,
+    authenticated: true,
   };
   next();
 };
@@ -24,10 +25,19 @@ const isAuthenticated = (req, res, next) => {
   }
 };
 
-app.use("/index", getUser, isAuthenticated);
+app.use(express.json());
+app.use("/index", morgan("combined"));
+app.use(express.static(path.join(__dirname, "public")));
+// Pour récupérer des fichiers comme des images par exemple
 
+app.use("/index", getUser, isAuthenticated);
 // je peux ajouter ou enlever en premier parametre un chemin
 // app.get('*", middleware) cela ciblera toutes les requetes get
+
+app.post("/index", (req, res) => {
+  console.log(req.body);
+  res.render("ok");
+});
 
 app.get("/index", (req, res) => {
   res.render("ok");
