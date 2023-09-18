2810023c110180365e178919784ae2e41834115e
D-D-f <defreitas-david@outlook.fr>
2023-09-18 21:39:53 +0200

    add

---
+++ js/classJavaScript.js
@@ -0,0 +1,13 @@
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  get reversedName() {
+    return this.name.split("").reverse().join("");
+  }
+}
+
+const perso = new Person("David");
+
+console.log(perso.reversedName);
