40fe830795c582a1a29b52647728dba18a3e7ecb
David <113393449+D-D-f@users.noreply.github.com>
2023-10-07 21:23:55 +0200

    Update index.test.js

--- js/index.test.js
+++ js/index.test.js
@@ -3,6 +3,10 @@ describe('Run test démo', () => {
         const a = 2 + 2;
         expect(a).toBe(4);
     })
-})
+
+    it('vérifier si la fonction retourne le prenom', () => {
+        expect(sayName('david').toBe('david');
+    })
+ })
 
 // Permet de vérifié si le résultat obtenu est bien celui attendu
